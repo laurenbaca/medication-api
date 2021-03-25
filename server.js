8b4cb5413eaf018a52b440f8d82be36568d8fca5
Lauren Baca <laurenbaca.dev@gmail.com>
2021-03-24 23:49:03 -0700

    working medication api

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const PORT = 8000
+
+app.use(cors())
+
+let drugs = {
+  'amlodipine': {
+    'brandName': 'norvasc',
+    'class': 'calcium channel blocker',
+    'indication': 'hypertension'
+  },
+  'zolpidem': {
+    'brandName': 'ambien',
+    'class': 'benzodiazepine',
+    'indication': 'insomnia'
+  },
+  'nitrofurantoin': {
+    'brandName': 'macrobid',
+    'class': 'urinary anti-infectives',
+    'indication': 'urinary tract infection'
+  },
+  'buspirone': {
+    'brandName': 'buspar',
+    'class': 'anxiolytic',
+    'indication': 'anti-anxiety'
+  },
+  'warfarin': {
+    'brandName': 'coumadin',
+    'class': 'anticoagulant',
+    'indication': 'blood clot reduction'
+  },
+  'unknown': {
+    'brandName': 'unknown',
+    'class': 'unknown',
+    'indication': 'unknown'
+  },
+
+}
+
+app.get('/', (request, response) => {
+  response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/drugs/:brandName', (request, response) => {
+  const brand = request.params.brandName.toLowerCase()
+  if(drugs[brand]){
+    response.json(drugs[brand])
+  }else{
+    response.json(drugs['unknown'])
+  }
+
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+})
