46cc9742c35200f7f9812d8044cfa5bde4be8ca2
Lauren Baca <laurenbaca.dev@gmail.com>
2021-03-25 00:03:10 -0700

    added port + procfile

--- server.js
+++ server.js
@@ -53,6 +53,6 @@ app.get('/api/drugs/:brandName', (request, response) => {
 
 })
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || PORT, () => {
   console.log(`Server running on ${PORT}`);
 })
